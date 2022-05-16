76f1053c7e28aa719694b3c9b8db4eafb5cee3d0
deepakaneesh7 <104065134+deepakaneesh7@users.noreply.github.com>
2022-05-16 19:46:09 +0530

    Add files via upload

---
+++ basictest.spec.js
@@ -0,0 +1,13 @@
+const { chromium, expect, default: test } = require ("@playwright/test")
+test('basic test', async () => {
+  const browser= await chromium.launch()
+  const page= await browser.newPage()
+  await page.goto('https://my.staging.anywhere.app/')
+  await page.fill("#user-email","sai@mailinator.com")
+  await page.fill("#user-password", "Test123123")
+  await page.click("#sign-in")
+  await expect(page.locator('#sign-in')).toHaveText('Sign in with email')
+  // await page.waitForTimeout(5000)
+  await page.screenshot({path : `test.png`})
+  await browser.close()
+});
